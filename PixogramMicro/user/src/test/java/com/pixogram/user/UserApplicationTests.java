be01a0d72d9f20fe1001b1c85f1cdc7d78c13d88
jagadeshreddy <jagadeshjy8898@gmail.com>
2019-09-24 10:44:49 +0530

    Update UserApplicationTests.java

--- PixogramMicro/user/src/test/java/com/pixogram/user/UserApplicationTests.java
+++ PixogramMicro/user/src/test/java/com/pixogram/user/UserApplicationTests.java
@@ -1,39 +1,55 @@
 package com.pixogram.user;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import com.pixogram.user.controller.UserController;
 import com.pixogram.user.entity.User;
+import com.pixogram.user.repo.UserRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserApplicationTests {
+@Autowired
+private static UserRepo controller;
 
-	private static UserController controller;
-	@BeforeClass
-	public static void setup()
-	{
-		controller = new UserController();
-	}
-	
-	@Test
-	public void contextLoads() {
-		
-	}
-	
-	@Test
-	public void checkReturnvalue()
-	{
-		User object=controller.getuser(1);
-	}
-	
-	
-	
-	
-	
+@Autowired
+UserRepo repo;
+@Test
+   public void userRepo4(){
+List<User> gfg = new ArrayList<User>();
 
+gfg=repo.findByUsernameAndPassword("demo","demo");
+assertEquals(gfg.get(0).getEmail(),"demo@gmail.com");
+   
 }
+
+@Test
+   public void myRepo4(){
+ArrayList<Optional> gfg = new ArrayList<Optional>();
+assertEquals(repo.findByUsernameAndPassword("0", "0"),gfg);
+   
+}
+@Test
+   public void myRepo(){
+      assertNotNull(repo.findAll());
+   }
+@Test
+   public void myRepo1(){
+
+assertNotNull(repo.findById(1));
+   }
+
